f1b92408f4594e163d2a808421a991f3e7646fc4
Jared Cohen <jaredcohe@gmail.com>
2014-11-08 22:02:56 -0500

    new computer

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dbaseeduApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dbaseeduApp
+ */
+angular.module('dbaseeduApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dbaseeduApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dbaseeduApp
+ */
+angular.module('dbaseeduApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
